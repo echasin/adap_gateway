cb7b4e9c29f0ed5726a91073d3c8306d67a59b8f
echasin <echasin@innvosolutions.com>
2016-11-01 23:09:41 -0400

    Added controller and html for carb dashboard demo

---
+++ src/main/webapp/app/entities/dashboard/dashboard.controller-carb.js
@@ -0,0 +1,31 @@
+(function() {
+	'use strict';
+
+	angular.module('adapGatewayApp').controller('DashboardControllerCarb',
+			DashboardControllerCarb);
+
+	DashboardControllerCarb.$inject = [ '$scope', '$state' ];
+
+	function DashboardControllerCarb($scope, $state) {
+
+		$scope.metric = {"name" : "NEW MESSAGES"}
+
+		$scope.names = [ {
+			"Name" : "Max Joe",
+			"City" : "Lulea",
+			"Country" : "Sweden"
+		}, {
+			"Name" : "Manish",
+			"City" : "Delhi",
+			"Country" : "India"
+		}, {
+			"Name" : "Koniglich",
+			"City" : "Barcelona",
+			"Country" : "Spain"
+		}, {
+			"Name" : "Wolski",
+			"City" : "Arhus",
+			"Country" : "Denmark"
+		} ];
+	}
+})();
