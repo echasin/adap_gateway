f009217a9c6c37b990fbb591ec2f33dfa07ac270
ali <ali@ali>
2016-11-22 10:17:08 +0200

    portfolioprojectmbr pagination

--- src/main/webapp/app/entities/portfolio/portfolio-edit.controller.js
+++ src/main/webapp/app/entities/portfolio/portfolio-edit.controller.js
@@ -5,9 +5,9 @@
         .module('adapGatewayApp')
         .controller('PortfolioEditController', PortfolioEditController);
 
-    PortfolioEditController.$inject = ['$timeout','$location', '$scope', '$stateParams', 'entity', 'Portfolio', 'Portfolioprojectmbr', 'Category', 'Subcategory', 'Recordtype','Account'];
+    PortfolioEditController.$inject = ['$timeout','$location', '$scope', '$stateParams', 'entity', 'Portfolio', 'Portfolioprojectmbr', 'Category', 'Subcategory', 'Recordtype','$state','Account','ParseLinks', 'AlertService', 'pagingParams', 'paginationConstants'];
 
-    function PortfolioEditController ($timeout,$location, $scope, $stateParams, entity, Portfolio, Portfolioprojectmbr, Category, Subcategory, Recordtype,Account) {
+    function PortfolioEditController ($timeout,$location, $scope, $stateParams, entity, Portfolio, Portfolioprojectmbr, Category, Subcategory, Recordtype,$state,Account,ParseLinks, AlertService, pagingParams, paginationConstants) {
         var vm = this;
 
         vm.portfolio = entity;
@@ -15,7 +15,7 @@
         vm.datePickerOpenStatus = {};
         vm.openCalendar = openCalendar;
         vm.save = save;
-        vm.portfolioprojectmbrs = Portfolioprojectmbr.query();
+        //vm.portfolioprojectmbrs = Portfolioprojectmbr.query();
         vm.categories = Category.query();
         vm.subcategories = Subcategory.query();
         vm.recordtypes = Recordtype.query();
@@ -51,6 +51,88 @@
 
         function openCalendar (date) {
             vm.datePickerOpenStatus[date] = true;
+        }
+        
+        
+        vm.loadPage = loadPage;
+        vm.predicate = pagingParams.predicate;
+        vm.reverse = pagingParams.ascending;
+        vm.transition = transition;
+        vm.itemsPerPage = paginationConstants.itemsPerPage;
+        vm.clear = clear;
+        vm.search = search;
+        vm.loadAll = loadAll;
+        vm.searchQuery = pagingParams.search;
+        vm.currentSearch = pagingParams.search;
+
+        loadAll();
+
+        function loadAll () {
+            if (pagingParams.search) {
+                PortfolioprojectmbrSearch.query({
+                    query: pagingParams.search,
+                    page: pagingParams.page - 1,
+                    size: vm.itemsPerPage,
+                    sort: sort()
+                }, onSuccess, onError);
+            } else {
+                Portfolioprojectmbr.query({
+                    page: pagingParams.page - 1,
+                    size: vm.itemsPerPage,
+                    sort: sort()
+                }, onSuccess, onError);
+            }
+            function sort() {
+                var result = [vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc')];
+                if (vm.predicate !== 'id') {
+                    result.push('id');
+                }
+                return result;
+            }
+            function onSuccess(data, headers) {
+                vm.links = ParseLinks.parse(headers('link'));
+                vm.totalItems = headers('X-Total-Count');
+                vm.queryCount = vm.totalItems;
+                vm.portfolioprojectmbrs = data;
+                vm.page = pagingParams.page;
+            }
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
+        }
+
+        function loadPage (page) {
+            vm.page = page;
+            vm.transition();
+        }
+
+        function transition () {
+            $state.transitionTo($state.$current, {
+                page: vm.page,
+                sort: vm.predicate + ',' + (vm.reverse ? 'asc' : 'desc'),
+                search: vm.currentSearch
+            });
+        }
+
+        function search (searchQuery) {
+            if (!searchQuery){
+                return vm.clear();
+            }
+            vm.links = null;
+            vm.page = 1;
+            vm.predicate = '_score';
+            vm.reverse = false;
+            vm.currentSearch = searchQuery;
+            vm.transition();
+        }
+
+        function clear () {
+            vm.links = null;
+            vm.page = 1;
+            vm.predicate = 'id';
+            vm.reverse = true;
+            vm.currentSearch = null;
+            vm.transition();
         }
     }
 })();
