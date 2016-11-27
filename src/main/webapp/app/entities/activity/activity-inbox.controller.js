141d884c90e657d89d90b6a67c64a5b628a462ec
ali <ali@ali>
2016-11-27 14:49:34 +0200

    new commit

--- src/main/webapp/app/entities/activity/activity-inbox.controller.js
+++ src/main/webapp/app/entities/activity/activity-inbox.controller.js
@@ -10,7 +10,6 @@
     function ActivityInboxController ($scope, $stateParams , $state, Activity, ActivitySearch, Recordtype, ParseLinks, AlertService) {
         var vm = this;
         
-        
         vm.activitiesBox=Activity.activitiesBox({id:$stateParams.params.id});
         vm.recordtype=Recordtype.get({id:$stateParams.params.id});
       
