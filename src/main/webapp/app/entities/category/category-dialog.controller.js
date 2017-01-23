41bfb9a8eccd4d73f60cdf5df6a33175f8a1e0a7
echasin <echasin@innvosolutions.com>
2017-01-22 22:58:53 -0800

    Removed "key" dependency from recordtype and category controllers. Changed database to adap_mrk and user postgres/postgres Simplified Left side naivgation for MRK deployment

--- src/main/webapp/app/entities/category/category-dialog.controller.js
+++ src/main/webapp/app/entities/category/category-dialog.controller.js
@@ -5,9 +5,9 @@
         .module('adapGatewayApp')
         .controller('CategoryDialogController', CategoryDialogController);
 
-    CategoryDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Category', 'Recordtype', 'Subcategory', 'Asset', 'Organization', 'Organizationorganizationmbr', 'Key', 'Project'];
+    CategoryDialogController.$inject = ['$timeout', '$scope', '$stateParams', '$uibModalInstance', 'entity', 'Category', 'Recordtype', 'Subcategory', 'Asset', 'Organization', 'Organizationorganizationmbr', 'Project'];
 
-    function CategoryDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Category, Recordtype, Subcategory, Asset, Organization, Organizationorganizationmbr, Key, Project) {
+    function CategoryDialogController ($timeout, $scope, $stateParams, $uibModalInstance, entity, Category, Recordtype, Subcategory, Asset, Organization, Organizationorganizationmbr, Project) {
         var vm = this;
 
         vm.category = entity;
@@ -20,7 +20,6 @@
         vm.assets = Asset.query();
         vm.organizations = Organization.query();
         vm.organizationorganizationmbrs = Organizationorganizationmbr.query();
-        vm.keys = Key.query();
         vm.projects = Project.query();
 
         $timeout(function (){
