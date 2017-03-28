259e88b3a625cbb07d7d6ff8447fac95127903a6
echasin <echasin@innvosolutions.com>
2017-03-27 21:32:14 -0700

    Updated adap.bat to access c: drive

--- adap.bat
+++ adap.bat
@@ -1,9 +1,10 @@
-START /D d:\Github\jhipster-registry-25 call mvn install -DskipTests spring-boot:run
+#REM Batch Deploy Adap Counter Measures
+START /D c:\Github\jhipster-registry-25 call mvn install -DskipTests spring-boot:run
 timeout /t 5 
-START /D d:\Github\adap_gateway call mvn install -DskipTests spring-boot:run
+START /D c:\Github\adap_gateway call mvn install -DskipTests spring-boot:run
 timeout /t 5 
-START /D d:\Github\adap_core call mvn install -DskipTests spring-boot:run
+START /D c:\Github\adap_core call mvn install -DskipTests spring-boot:run
 timeout /t 5 
-START /D d:\Github\adap_risk call mvn -Pprod install -DskipTests spring-boot:run
-timeout /t 5 
-#START /D d:\Github\adap_report 
+START /D c:\Github\adap_risk call mvn -Pprod install -DskipTests spring-boot:run
+#timeout /t 5 
+#START /D c:\Github\adap_report 
