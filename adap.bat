c12f11c76a31402034f25cf9e25919dfd463cd50
echasin <echasin@innvosolutions.com>
2017-03-27 21:17:14 -0700

    Update scenario pages eliminated alert on Scenario and updated scenario-detail to inspinia format

--- adap.bat
+++ adap.bat
@@ -1,1 +1,9 @@
-start cmd mvn install spring-boot:run
+START /D d:\Github\jhipster-registry-25 call mvn install -DskipTests spring-boot:run
+timeout /t 5 
+START /D d:\Github\adap_gateway call mvn install -DskipTests spring-boot:run
+timeout /t 5 
+START /D d:\Github\adap_core call mvn install -DskipTests spring-boot:run
+timeout /t 5 
+START /D d:\Github\adap_risk call mvn -Pprod install -DskipTests spring-boot:run
+timeout /t 5 
+#START /D d:\Github\adap_report 
