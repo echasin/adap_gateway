bfb70ed022b30967fe37fd582b465a16bbfbb5e4
echasin <echasin@innvosolutions.com>
2017-03-29 07:15:19 -0700

    Update adap_c.bat to use mvnw

--- adap_c.bat
+++ adap_c.bat
@@ -1,10 +1,15 @@
 #REM Batch Deploy Adap Counter Measures
-START /D c:\Github\jhipster-registry-25 call mvnw -DskipTests spring-boot:run
-timeout /t 5 
-START /D c:\Github\adap_gateway call mvnw -DskipTests spring-boot:run
-timeout /t 5 
-START /D c:\Github\adap_core call mvnw -DskipTests spring-boot:run
-timeout /t 5 
-START /D c:\Github\adap_risk call mvnw -DskipTests spring-boot:run
+
+START /D c:\Github\jhipster-registry call mvnw -Dmaven.test.skip=true
+timeout /t 15 
+
+START /D c:\Github\adap_gateway call mvnw -Dmaven.test.skip=true
+timeout /t 15 
+
+START /D c:\Github\adap_core call mvnw -Dmaven.test.skip=true
+timeout /t 15 
+
+START /D c:\Github\adap_risk call mvnw -Dmaven.test.skip=true
 #timeout /t 5 
+
 #START /D c:\Github\adap_report 
